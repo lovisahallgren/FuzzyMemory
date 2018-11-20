b470bc9ebd580837bf96e30976045eb1a60dc159
Lovisa Hallgren <lovisahallgren1@gmail.com>
2018-11-20 13:21:39 +0100

    created divs of images in javascript

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 'use strict';
 
-const ingredients = [
-  { name: 'Apple', color: '#ffb3ba', image: './images/apple.png' },
-  { name: 'Banana', color: '#FFF9C7', image: './images/banana.png' },
-  { name: 'Carrot', color: '#ffdfba', image: './images/carrot.png' },
-  { name: 'Strawberry', color: '#FF4859', image: './images/strawberry.png' },
-  { name: 'Orange', color: '#FF9145', image: './images/orange.png' },
-  { name: 'Grapes', color: '#D7BDFF', image: './images/grapes.png' },
-  { name: 'Kiwi', color: '#CBFF7B', image: './images/kiwi.png' }
+const images = [
+  {image: './images/sheldon.jpeg' },
+  {image: './images/leonard.jpg' },
+  {image: './images/penny.jpg' },
+  {image: './images/howard.jpg' },
+  {image: './images/stuart.jpg' },
+  {image: './images/wil.jpg' },
+  {image: './images/amy.jpg' },
+  {image: './images/raj.jpg' },
+  {image: './images/bernadette.jpg' },
+  {image: './images/proton.jpg' }
 ]
 
-// const createIngredient = (icon, title, color) => {
-//   return `
-//   <div class="ingredient-item" data-color=${color}>
-//     <img src=${icon} />
-//     <p> ${title} </p>
-//   </div>`
-// }
+const board = document.querySelector('.board');
+
+const createCard = (image) => {
+  return `
+  <div class="card">
+    <img src=${image} />
+  </div>`
+};
+
+
+images.forEach((image) => {
+  board.innerHTML += createCard(image['image']);
+});
