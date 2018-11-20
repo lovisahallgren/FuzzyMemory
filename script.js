628228d36a41f3ff6823126fff1a046ed42522c3
Lovisa Hallgren <lovisahallgren1@gmail.com>
2018-11-20 10:23:12 +0100

    created files for the task

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const ingredients = [
+  { name: 'Apple', color: '#ffb3ba', image: './images/apple.png' },
+  { name: 'Banana', color: '#FFF9C7', image: './images/banana.png' },
+  { name: 'Carrot', color: '#ffdfba', image: './images/carrot.png' },
+  { name: 'Strawberry', color: '#FF4859', image: './images/strawberry.png' },
+  { name: 'Orange', color: '#FF9145', image: './images/orange.png' },
+  { name: 'Grapes', color: '#D7BDFF', image: './images/grapes.png' },
+  { name: 'Kiwi', color: '#CBFF7B', image: './images/kiwi.png' }
+]
+
+// const createIngredient = (icon, title, color) => {
+//   return `
+//   <div class="ingredient-item" data-color=${color}>
+//     <img src=${icon} />
+//     <p> ${title} </p>
+//   </div>`
+// }
