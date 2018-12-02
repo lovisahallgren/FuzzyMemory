a36e94057f51093235fb27c152e5688c8e469e61
Lovisa Hallgren <lovisahallgren1@gmail.com>
2018-12-02 13:24:58 +0100

    started again

--- script.js
+++ script.js
@@ -10,19 +10,25 @@ const images = [
   {image: './images/amy.jpg' },
   {image: './images/raj.jpg' },
   {image: './images/bernadette.jpg' },
-  {image: './images/proton.jpg' }
-]
+  {image: './images/proton.jpg' },
+];
 
 const board = document.querySelector('.board');
 
-const createCard = (image) => {
+
+//creates card
+const createCard = (card) => {
   return `
-  <div class="card">
-    <img src=${image} />
+  <div class="card" data-id=${card}>
   </div>`
 };
-
 
 images.forEach((image) => {
   board.innerHTML += createCard(image['image']);
 });
+
+
+//checks if it's a pair
+const pair = (first, second) => {
+  return first === second;
+}
